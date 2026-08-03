e1c84776ee73d0ec3109905726e98fb08990e7db
sba25g10010-star <sba25g10010@edu.sba.ac.jp>
2026-08-04 02:57:10 +0900

    先生システムの作成

--- GameJam/Assets/SummerGameJam/Script/QuestionManager.cs
+++ GameJam/Assets/SummerGameJam/Script/QuestionManager.cs
@@ -1,11 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class QuestionManager : MonoBehaviour
 {
     private List<QuestionSO> questionDatabase = new List<QuestionSO>();
     private QuestionSO currentQuestion;
+
     private bool isGameOver = false;
+    [Header("先生管理クラスの参照")]
+    [SerializeField] private TeacherManager teacherManager;
+
 
     [Header("UIクラスの参照")]
     [SerializeField] private QuestionUI gameUI;
@@ -42,17 +47,63 @@ public class QuestionManager : MonoBehaviour
     {
         if (isGameOver) return;
 
+
         float randomValue = Random.Range(0f, 100f);
 
         if (randomValue <= currentQuestion.failureChance)
         {
-            Debug.Log($"失敗！確率 {currentQuestion.failureChance}% に対し {randomValue:F1} を引いた");
+            Debug.Log(
+                $"失敗！確率 {currentQuestion.failureChance}% に対し " +
+                $"{randomValue:F1} を引いた"
+            );
+
             isGameOver = true;
             gameUI.ShowGameOverUI();
         }
         else
         {
-            Debug.Log($"成功！確率 {currentQuestion.failureChance}% に対し {randomValue:F1} で回避");
+            Debug.Log(
+                $"成功！確率 {currentQuestion.failureChance}% に対し " +
+                $"{randomValue:F1} で回避"
+            );
+
+            NextTurn();
+        }
+    }
+    /// <summary>
+    /// 現在の先生に問題を解いてもらう
+    /// </summary>
+    public void OnTeacherButton()
+    {
+        if (isGameOver) return;
+
+        if (teacherManager.IsCorrectTeacher(currentQuestion))
+        {
+            Debug.Log("先生の得意な問題なので成功！");
+            NextTurn();
+            return;
+        }
+
+        float randomValue = Random.Range(0f, 100f);
+
+        if (randomValue <= currentQuestion.failureChance)
+        {
+            Debug.Log(
+                $"先生が失敗！確率 {currentQuestion.failureChance}% に対し " +
+                $"{randomValue:F1} を引いた"
+            );
+
+            isGameOver = true;
+            gameUI.ShowGameOverUI();
+        }
+        else
+        {
+            Debug.Log(
+                $"先生の不得意な問題だが成功！確率 " +
+                $"{currentQuestion.failureChance}% に対し " +
+                $"{randomValue:F1} で回避"
+            );
+
             NextTurn();
         }
     }
@@ -67,4 +118,6 @@ public class QuestionManager : MonoBehaviour
         Debug.Log("スルーして次の問題へ");
         NextTurn();
     }
+
+
 }
