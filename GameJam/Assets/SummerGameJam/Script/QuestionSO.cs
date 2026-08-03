d91b12f7064abfc68d2e39e2d9c6e8a451d3166b
motokjy <kjtmtk@icloud.com>
2026-08-03 15:01:34 +0900

    AIに聞いたら一定確率で失敗するボタンが完成

---
+++ GameJam/Assets/SummerGameJam/Script/QuestionSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Question" ,menuName = "Game/CreateQuestion")]
+public class QuestionSO : ScriptableObject
+{
+    public string questionName; //問題の名前
+
+    [Range(0,100)]
+    public int failureChance; //問題が失敗する確率
+}
