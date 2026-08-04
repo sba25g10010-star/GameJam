dcb77fae0218684cf66f3a2c530c76d949ff5cdf
motokjy <kjtmtk@icloud.com>
2026-08-04 11:15:37 +0900

    先生のSOに二つのストレスアップを追加

--- GameJam/Assets/SummerGameJam/Script/TeacherSO.cs
+++ GameJam/Assets/SummerGameJam/Script/TeacherSO.cs
@@ -6,7 +6,8 @@ public class TeacherSO : ScriptableObject
     public string teacherName; //先生の名前
     public string hobby; // 得意なこと
     public Sprite teacherImage; //先生の画像
-    public int stlessUp; //ストレスアップ
+    public int correctStlessUp; //成功時ストレスアップ
+    public int missStlessUp; //失敗時ストレスアップ
     public int efficiencyUp; //効率アップ
     public int efficiencyDown; //効率ダウン
     public int intimacy; //親密度
